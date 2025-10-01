e6ffd928ed517f814b82192e7d7f395a361f704f
ZapataAnderson21 <u21208915@utp.edu.pe>
2025-10-01 13:08:48 -0500

    fix: set default values for status fields in Emergency, Project, and Request tables

---
+++ prisma/migrations/20251001180738_change_the_default_values_for_the_project_emergency_and_request_table_statuses/migration.sql
@@ -0,0 +1,8 @@
+-- AlterTable
+ALTER TABLE "Emergency" ALTER COLUMN "status" SET DEFAULT 'Activo';
+
+-- AlterTable
+ALTER TABLE "Project" ALTER COLUMN "status" SET DEFAULT 'Activo';
+
+-- AlterTable
+ALTER TABLE "Request" ALTER COLUMN "status" SET DEFAULT 'Pendiente';
