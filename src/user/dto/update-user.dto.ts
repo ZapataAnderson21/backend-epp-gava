2e2e5af750150e320e2134ad282f74d4d3b8241a
ZapataAnderson21 <u21208915@utp.edu.pe>
2026-01-30 12:53:43 -0500

    feat(user): enhance UpdateUserDto with password validation rules

--- src/user/dto/update-user.dto.ts
+++ src/user/dto/update-user.dto.ts
@@ -1,4 +1,16 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateUserDto } from './create-user.dto';
+import { ApiPropertyOptional } from '@nestjs/swagger';
+import { IsOptional, MinLength, Matches, IsString, IsNotEmpty } from 'class-validator';
 
-export class UpdateUserDto extends PartialType(CreateUserDto) {}
+export class UpdateUserDto extends PartialType(CreateUserDto) {
+	@ApiPropertyOptional({ minLength: 8, example: 'Secreta#123' })
+	@IsOptional()
+	@IsString()
+	@MinLength(8, { message: "\nLa contraseña debe tener al menos 8 caracteres." })
+	@Matches(/^(?=.*[A-Z])(?=.*\d)(?=.*[^A-Za-z0-9\s]).+$/, {
+		message:
+			"\nLa contraseña debe contener al menos una mayúscula, un número y un caracter especial.",
+	})
+	password?: string;
+}
